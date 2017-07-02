07a605aa3bac2c9403c6e03179608718180ded31
freak07 <michalechner92@googlemail.com>
2017-07-02 19:51:09 +0200

    Revert "perf/core: Fix event inheritance on fork()"
    This reverts commit 995587ff274d7233d03d78300ef60e9a25f0ecaa.

--- kernel/events/core.c
+++ kernel/events/core.c
@@ -9483,7 +9483,7 @@ static int perf_event_init_context(struct task_struct *child, int ctxn)
 		ret = inherit_task_group(event, parent, parent_ctx,
 					 child, ctxn, &inherited_all);
 		if (ret)
-			goto out_unlock;
+			break;
 	}
 
 	/*
@@ -9499,7 +9499,7 @@ static int perf_event_init_context(struct task_struct *child, int ctxn)
 		ret = inherit_task_group(event, parent, parent_ctx,
 					 child, ctxn, &inherited_all);
 		if (ret)
-			goto out_unlock;
+			break;
 	}
 
 	raw_spin_lock_irqsave(&parent_ctx->lock, flags);
@@ -9527,7 +9527,6 @@ static int perf_event_init_context(struct task_struct *child, int ctxn)
 	}
 
 	raw_spin_unlock_irqrestore(&parent_ctx->lock, flags);
-out_unlock:
 	mutex_unlock(&parent_ctx->mutex);
 
 	perf_unpin_context(parent_ctx);
